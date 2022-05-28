fa4ae92df6f315a1be749815af8f15998fed2287
TungLuu <tungluu2203@kaist.ac.kr>
2022-05-28 16:50:49 +0900

    Remove testing with single value of epsilon

--- evaluate_learned_policy.py
+++ evaluate_learned_policy.py
@@ -36,7 +36,7 @@ parser.add_argument('--adv_version', type=str, default='a1_d1')
 
 SUPPORTED_ATTACKS = ['random', 'critic_normal', 'actor_mad']
 parser.add_argument('--attack_type', type=str, default='random', choices=SUPPORTED_ATTACKS)
-parser.add_argument('--attack_epsilon', type=float, default=1e-4)
+parser.add_argument('--attack_epsilon', type=float, default=None)
 parser.add_argument('--attack_epsilon_list', type=float, default=1e-4, nargs='+')
 parser.add_argument('--attack_iteration', type=int, default=10)
 
@@ -51,9 +51,9 @@ args = parser.parse_args()
 """
 ##### Functions used to evaluate
 """
-def eval_clean_env(args_):
-    algo, env, start_seed, args = args_
-    n_trials = args.n_eval_episodes
+def eval_clean_env(params):
+    algo, env, start_seed, params = params
+    n_trials = params.n_eval_episodes
 
     episode_rewards = []
     for i in tqdm(range(n_trials)):
@@ -79,20 +79,20 @@ def eval_clean_env(args_):
     return unorm_score
 
 
-def eval_env_under_attack(args_):
-    algo, env, start_seed, args = args_
-    n_trials = args.n_eval_episodes
+def eval_env_under_attack(params):
+    algo, env, start_seed, params = params
+    n_trials = params.n_eval_episodes
 
     # Set seed
-    torch.manual_seed(args.seed)
-    np.random.seed(args.seed)
+    torch.manual_seed(params.seed)
+    np.random.seed(params.seed)
 
-    attack_type = args.attack_type
-    attack_epsilon = args.attack_epsilon
-    attack_iteration = args.attack_iteration
+    attack_type = params.attack_type
+    attack_epsilon = params.attack_epsilon
+    attack_iteration = params.attack_iteration
     attack_stepsize = attack_epsilon / attack_iteration
     print("[INFO] Using %s attack: eps=%f, n_iters=%d, sz=%f" %
-          (args.attack_type.upper(), attack_epsilon, attack_iteration, attack_stepsize))
+          (params.attack_type.upper(), attack_epsilon, attack_iteration, attack_stepsize))
 
     def attack(state, type, attack_epsilon=None, attack_iteration=None, attack_stepsize=None):
         if type in ['random']:
@@ -155,24 +155,24 @@ def eval_env_under_attack(args_):
     return unorm_score
 
 
-def eval_multiprocess_wrapper(algo, func, env_list, args):
-    n_trials_per_each = int(args.n_eval_episodes / args.n_processes)
-    n_trials_for_last = n_trials_per_each if args.n_eval_episodes % args.n_processes == 0 else \
-        n_trials_per_each + args.n_eval_episodes % args.n_processes
+def eval_multiprocess_wrapper(algo, func, env_list, params):
+    n_trials_per_each = int(params.n_eval_episodes / params.n_processes)
+    n_trials_for_last = n_trials_per_each if params.n_eval_episodes % params.n_processes == 0 else \
+        n_trials_per_each + params.n_eval_episodes % params.n_processes
 
     args_list = []
-    for i in range(args.n_processes):
-        args_ = copy.deepcopy(args)
+    for i in range(params.n_processes):
+        params_tmp = copy.deepcopy(params)
 
-        if i == args.n_processes - 1:  # last iteration
-            args_.n_eval_episodes = n_trials_for_last
+        if i == params_tmp.n_processes - 1:  # last iteration
+            params_tmp.n_eval_episodes = n_trials_for_last
         else:
-            args_.n_eval_episodes = n_trials_per_each
+            params_tmp.n_eval_episodes = n_trials_per_each
 
         start_seed = n_trials_per_each * i + 1
-        args_list.append((algo, env_list[i], start_seed, args_))
+        args_list.append((algo, env_list[i], start_seed, params_tmp))
 
-    with mp.Pool(args.n_processes) as pool:
+    with mp.Pool(params.n_processes) as pool:
         unorm_score = pool.map(func, args_list)
         unorm_score = np.mean(unorm_score)
 
@@ -297,15 +297,12 @@ def main(args):
         print("Noise env: unorm = %.3f, norm = %.2f" % (unorm_score_noise, norm_score_noise))
         # print("=> Time(s) for evaluation: %.3f" % (time.time() - start))
 
-    if len(args.attack_epsilon_list) > 1:
-        for i, attack_epsilon in enumerate(args.attack_epsilon_list):
-            if i == 0:
-                disable_clean = args.disable_clean
-            else:
-                disable_clean = True
-            eval_func(attack_epsilon, disable_clean)
-    else:
-        eval_func(args.attack_epsilon, args.disable_clean)
+    for i, attack_epsilon in enumerate(args.attack_epsilon_list):
+        if i == 0:
+            disable_clean = args.disable_clean
+        else:
+            disable_clean = True
+        eval_func(attack_epsilon, disable_clean)
 
 
 
