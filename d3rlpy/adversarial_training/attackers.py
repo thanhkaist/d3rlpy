50a286793de38741a77054ca4ccd477f7571a74d
Thanh Nguyen <thanhkaist@github.com>
2022-06-06 22:46:13 +0900

    Add SGLD

--- d3rlpy/adversarial_training/attackers.py
+++ d3rlpy/adversarial_training/attackers.py
@@ -1,3 +1,4 @@
+from re import M
 import torch
 import torch.nn.functional as F
 
@@ -62,7 +63,7 @@ def critic_normal_attack(x, _policy, _q_func, epsilon, num_steps, step_size, _ob
 
 
 def actor_mad_attack(x, _policy, _q_func, epsilon, num_steps, step_size, _obs_min, _obs_max,
-                     scaler=None):
+                     scaler=None, optimizer="PGD"):
     """" NOTE: x must be un-normalized """""
 
     assert isinstance(x, torch.Tensor), "input x must be tensor."
@@ -78,25 +79,62 @@ def actor_mad_attack(x, _policy, _q_func, epsilon, num_steps, step_size, _obs_mi
     adv_x = adv_x + noise                                          # Add noise in `normalized space`
 
     # TODO: Optimized with PGD
-    for _ in range(num_steps):
-        adv_x_clone = adv_x.clone().detach()    # normalized
-        adv_x.requires_grad = True
 
-        adv_a = _policy(adv_x)
+    if optimizer in ["PGD",]:
+        for _ in range(num_steps):
+            adv_x_clone = adv_x.clone().detach()    # normalized
+            adv_x.requires_grad = True
 
-        cost = F.mse_loss(adv_a, gt_action)
+            adv_a = _policy(adv_x)
 
-        grad = torch.autograd.grad(cost, adv_x, retain_graph=False, create_graph=False)[0]
+            cost = F.mse_loss(adv_a, gt_action)
 
-        adv_x = adv_x_clone + step_size * grad
+            grad = torch.autograd.grad(cost, adv_x, retain_graph=False, create_graph=False)[0]
 
-        delta = torch.clamp(adv_x - ori_state_tensor, min=-epsilon, max=epsilon)
-        adv_x = adv_x_clone + delta         # This is adversarial example
+            adv_x = adv_x_clone + step_size * grad
 
-        # This clamp is performed in ORIGINAL scale
-        adv_x = scaler.reverse_transform(adv_x)
-        adv_x = clamp(adv_x, _obs_min, _obs_max).detach()
-        adv_x = scaler.transform(adv_x)
+            delta = torch.clamp(adv_x - ori_state_tensor, min=-epsilon, max=epsilon)
+            adv_x = adv_x_clone + delta         # This is adversarial example
+    elif optimizer in ["SGLD",]:
+
+        ##########################################
+        step_eps = epsilon/num_steps
+        # upper and lower bounds for clipping
+        adv_ub = ori_state_tensor + epsilon
+        adv_lb = ori_state_tensor - epsilon
+        # add uniform noise beween +/- scaled_robust_eps
+        # SGLD noise factor. We set (inverse) beta=1e-5 as gradients are relatively small here.
+        beta = 1e-5
+        noise_factor = torch.sqrt(2 * step_eps) * beta
+        noise = torch.randn_like(ori_state_tensor) * noise_factor
+        # First SGLD step, the gradient is 0, so only need to add noise. Project to Linf box.
+        adv_x = (ori_state_tensor.clone() + noise.sign() * step_eps).detach().requires_grad_()
+        # and clip into the upper and lower bounds (not necessary for now as we use uniform noise)
+        # adv_phi = torch.max(adv_phi, adv_lb)
+        # adv_phi = torch.min(adv_phi, adv_ub)
+        for i in range(num_steps):
+            # Find a nearby state adv_phi that maximize the difference
+            adv_loss = (_policy(adv_x) - gt_action).pow(2).mean()
+            # Need to clear gradients before the backward() for policy_loss
+            grad = torch.autograd.grad(adv_loss, adv_x, retain_graph=False, create_graph=False)[0]
+            # Reduce noise at every step. We start at step 2.
+            noise_factor = torch.sqrt(2 * step_eps) * beta / (i+2)
+            # Project noisy gradient to step boundary.
+            update = (grad + noise_factor * torch.randn_like(adv_x)).sign() * step_eps
+            adv_x = adv_x + update
+            # clip into the upper and lower bounds
+            adv_x = torch.max(adv_x, adv_lb)
+            adv_x = torch.min(adv_x, adv_ub).detach().requires_grad_()
+            # see how much the difference is
+            # self._meter.update('sgld_act_diff', (adv_phi - phi).abs().sum().item())
+            # We want to minimize the loss
+            # action_reg_loss = (self.network.actor(adv_phi) - action).pow(2).mean()
+
+            # This clamp is performed in ORIGINAL scale
+            adv_x = scaler.reverse_transform(adv_x)
+            adv_x = clamp(adv_x, _obs_min, _obs_max).detach()
+            adv_x = scaler.transform(adv_x)
+        
 
     perturbed_state = scaler.reverse_transform(adv_x)  # un-normalized
     return perturbed_state
