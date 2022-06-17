44900db5963fb8e868aa731b40ec8a725a44405c
Thanh Nguyen <thanhkaist@github.com>
2022-06-17 18:29:14 +0900

    Add debug information to evaluation scripts

--- d3rlpy/adversarial_training/eval_utility.py
+++ d3rlpy/adversarial_training/eval_utility.py
@@ -1,15 +1,18 @@
 import os
 import json
+import pdb
 import time
 import copy
 from tqdm import tqdm
 
 import torch
-from torch import multiprocessing as mp
+from torch import multiprocessing as mp, true_divide
 
 import numpy as np
 from .utility import tensor
 from .attackers import random_attack, critic_normal_attack, actor_mad_attack
+
+DEBUG= False
 
 def make_sure_type_is_float32(x):
     assert isinstance(x, np.ndarray)
@@ -21,6 +24,7 @@ def make_sure_type_is_float32(x):
 ##### Functions used to evaluate
 """
 def eval_clean_env(params):
+    print("EVALUATING ON CLEAN EVALUATION")
     rank, algo, env, start_seed, params = params
     n_trials = params.n_eval_episodes
 
@@ -50,12 +54,14 @@ def eval_clean_env(params):
 
 
 def eval_env_under_attack(params):
+    global DEBUG
     rank, algo, env, start_seed, params = params
     n_trials = params.n_eval_episodes
 
     # Set seed
     torch.manual_seed(params.seed)
     np.random.seed(params.seed)
+    # import pdb; pdb.set_trace()
 
     attack_type = params.attack_type
     attack_epsilon = params.attack_epsilon
@@ -70,7 +76,7 @@ def eval_env_under_attack(params):
         return the perturbed state, it must be un-normalized
         """""
         state_tensor = tensor(state, algo._impl.device)
-
+        # import pdb;pdb.set_trace()
         # Important: inside the attack functions, the state is assumed already normalized
         state_tensor = algo.scaler.transform(state_tensor)
 
@@ -81,6 +87,7 @@ def eval_env_under_attack(params):
             )
 
         elif type in ['critic_normal']:
+
             perturb_state = critic_normal_attack(
                 state_tensor, algo._impl._policy, algo._impl._q_func,
                 attack_epsilon, attack_iteration, attack_stepsize,
@@ -98,12 +105,24 @@ def eval_env_under_attack(params):
 
         else:
             raise NotImplementedError
-
+        assert np.linalg.norm(perturb_state.cpu()-state_tensor.cpu(),np.inf,1) < attack_epsilon + 1e-5, f"Over-epsilon {np.linalg.norm(perturb_state.cpu()-state_tensor.cpu(),np.inf,1) } "
+        
+        # if np.linalg.norm(perturb_state.cpu()-state_tensor.cpu(),np.inf,1) > attack_epsilon + 1e-5:
+        #     import pdb;pdb.set_trace()
+        #     print("ABC")
+        
         # De-normalize state for return in original scale
         perturb_state = algo.scaler.reverse_transform(perturb_state)
+        
         return perturb_state.squeeze().cpu().numpy()
 
     episode_rewards = []
+
+    if DEBUG:
+        act_diff_norm_inf = []
+        act_diff_norm_2 = []
+        state_diff_norm_inf = []
+        state_diff_norm_2 = []
     for i in tqdm(range(n_trials), disable=(rank != 0), desc="{} attack".format(attack_type.upper())):
         if start_seed is None:
             env.seed(i)
@@ -113,15 +132,34 @@ def eval_env_under_attack(params):
 
         episode_reward = 0.0
 
+        if DEBUG:
+            act_diff_inf = []
+            act_diff_2 = []
+            state_diff_inf = []
+            state_diff_2 = []
+
+
         while True:
             # take action
             state = make_sure_type_is_float32(state)
+            
+            if DEBUG:
+                origin_state = state.copy()
+                gt_action = algo.predict([state])[0]
+                
+
+
             state = perturb(
                 state,
                 attack_type, attack_epsilon, params.attack_iteration, attack_stepsize,
                 optimizer=params.optimizer
             )
             action = algo.predict([state])[0]
+            if DEBUG:
+                act_diff_inf.append(np.linalg.norm(action-gt_action,np.inf))
+                act_diff_2.append(np.linalg.norm(action-gt_action))
+                state_diff_inf.append(np.linalg.norm(state-origin_state,np.inf))
+                state_diff_2.append(np.linalg.norm(state-origin_state))
 
             state, reward, done, _ = env.step(action)
 
@@ -130,7 +168,18 @@ def eval_env_under_attack(params):
             if done:
                 break
         episode_rewards.append(episode_reward)
-
+        if DEBUG:
+            act_diff_norm_inf.append(np.mean(np.array(act_diff_inf)))
+            act_diff_norm_2.append(np.mean(np.array(act_diff_2)))
+            state_diff_norm_inf.append(np.mean(np.array(state_diff_inf)))
+            state_diff_norm_2.append(np.mean(np.array(state_diff_2)))
+    if DEBUG:
+        with open(f"Eval_debug_{attack_type}.txt") as f:
+            f.write("Active:"+ ",".join([str(i) for i in episode_rewards])+"\n")
+            f.write("Act_diff_inf:"+ ",".join([str(i) for i in act_diff_norm_inf])+"\n")
+            f.write("Act_diff_2:"+ ",".join([str(i) for i in act_diff_norm_2])+"\n")
+            f.write("state_diff_inf:"+ ",".join([str(i) for i in state_diff_norm_inf])+"\n")
+            f.write("state_diff_2:"+ ",".join([str(i) for i in state_diff_norm_2])+"\n")
     unorm_score = float(np.mean(episode_rewards))
     return unorm_score
 
